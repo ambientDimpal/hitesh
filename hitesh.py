154006a50b9c938b80087ac64fc64e27a0dcd48d
hitesh <hkj@example.com>
2025-05-29 11:51:10 +0530

    python code

---
+++ hitesh.py
@@ -0,0 +1,13 @@
+# Simple Python program
+
+# Print a greeting
+print("Hello, World!")
+
+# Add two numbers
+a = 5
+b = 3
+sum = a + b
+
+# Print the result
+print("The sum of", a, "and", b, "is", sum)
+
